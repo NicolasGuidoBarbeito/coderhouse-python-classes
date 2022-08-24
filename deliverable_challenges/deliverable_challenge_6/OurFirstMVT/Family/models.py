d200d73c3f1c50e6b2ea4f081a36d7b060e48baa
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2022-08-23 23:06:20 -0300

    :construction: Add Family Member model

--- deliverable_challenges/deliverable_challenge_6/OurFirstMVT/Family/models.py
+++ deliverable_challenges/deliverable_challenge_6/OurFirstMVT/Family/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class FamilyMember(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField()
+    birthdate = models.DateField()
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
